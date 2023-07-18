958e0445d3dd1fce00847a29b97539d21f109d65
Dolores Crazover <doloresnetweb@gmail.com>
2023-07-17 21:24:05 -0400

    settings updated

--- dailydone_project/settings.py
+++ dailydone_project/settings.py
@@ -31,9 +31,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
+# DEBUG = 'RENDER' not in os.environ
 
-
-DEBUG = 'RENDER' not in os.environ
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
