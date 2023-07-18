b1aa9a9964c64c7dca80d5e1ae4815c10d661218
Dolores Crazover <doloresnetweb@gmail.com>
2023-07-17 23:57:05 -0400

    ready to deploy

--- dailydone_project/settings.py
+++ dailydone_project/settings.py
@@ -30,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 # DEBUG = 'RENDER' not in os.environ
 
-# DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
